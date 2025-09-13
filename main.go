8175f3d5b6013070515c0bf58070ebf59ac5e9a2
TenLinks20 <uzonduberto94@gmail.com>
2025-09-13 16:32:30 +0200

    metrics and reset

--- main.go
+++ main.go
@@ -3,21 +3,34 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	srv := http.Server{
+	srv := &http.Server{
 		Addr: ":" + port,
 		Handler: mux,
 	}
+	apiCfg := &apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	fileHandler := http.FileServer(http.Dir(filepathRoot))
+	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(fileHandler)))
 
-	
+	mux.Handle("/healthz", http.HandlerFunc(handlerReadiness))
+	mux.Handle("/metrics", http.HandlerFunc(apiCfg.handlerMetrics))
+	mux.Handle("/reset", http.HandlerFunc(apiCfg.handlerReset))
+
+
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
 }
