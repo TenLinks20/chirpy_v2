77955ebf029f5596b640e8d6721178865deadcc5
TenLinks20 <uzonduberto94@gmail.com>
2025-09-13 10:43:48 +0200

    basic setup

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/TenLinks20/chirpy_v2
+
+go 1.24.2
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	srv := http.Server{
+		Addr: ":" + port,
+		Handler: mux,
+	}
+
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+
+	
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
+}
